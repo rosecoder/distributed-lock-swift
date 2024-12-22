a2d616403d2949791fd8be7bb41c353d51441092
Gustaf Rosenblad <gustaf@ccode.se>
2024-12-22 12:13:41 +0100

    feat: Initial commit

---
+++ Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version: 6.0
+import PackageDescription
+
+let package = Package(
+    name: "distributed-lock",
+    platforms: [
+        .macOS(.v15),
+    ],
+    products: [
+        .library(name: "DistributedLock", targets: ["DistributedLock"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-log.git", from: "1.4.2"),
+        .package(url: "https://github.com/apple/swift-distributed-tracing.git", from: "1.1.0"),
+    ],
+    targets: [
+        .target(
+            name: "DistributedLock",
+            dependencies: [
+                .product(name: "Logging", package: "swift-log"),
+                .product(name: "Tracing", package: "swift-distributed-tracing"),
+            ]
+        ),
+    ]
+)
